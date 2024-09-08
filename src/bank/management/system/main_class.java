855647a2c35fd99dab24c308c5dc947f5740845b
Pueeeth <punithreddy417@gmail.com>
2024-09-08 12:53:08 +0530

    Login,signup connected with database,signup2 and signup3 connected with database,main_class(atm and buttons),Withdrawl and BalanceEnquiry.

--- src/bank/management/system/main_class.java
+++ src/bank/management/system/main_class.java
@@ -87,6 +87,12 @@ public class main_class extends JFrame implements ActionListener {
             setVisible(false);
         } else if (e.getSource() == b7) {
             System.exit(0);
+        } else if (e.getSource() == b2) {
+            new Withdrawl(pin);
+            setVisible(false);
+        } else if (e.getSource() == b6) {
+            new BalanceEnquiry(pin);
+            setVisible(false);
         }
     }
 
