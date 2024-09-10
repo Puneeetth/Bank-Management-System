1d4fb2004516336d09df294cfd5b3d7a9215aecd
Pueeeth <punithreddy417@gmail.com>
2024-09-10 19:38:55 +0530

    added mini statement and completed the project

--- src/bank/management/system/FastCash.java
+++ src/bank/management/system/FastCash.java
@@ -102,7 +102,8 @@ public class FastCash extends JFrame implements ActionListener {
                           balance -= Integer.parseInt(resultSet.getString("amount"));
 
                       }
-                  }String num = "17";
+                  }
+                  String num = "17";
 
                   if(e.getSource() != b7 && balance < Integer.parseInt(amount)){
                       JOptionPane.showMessageDialog(null,"Insufficient Balance");
